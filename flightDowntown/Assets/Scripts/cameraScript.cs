f17958df829bb54cd2938688e967fd981d53ffa6
CarlyWilliams22 <57012119+CarlyWilliams22@users.noreply.github.com>
2020-09-22 20:53:13 -0400

    Hero, Ceiling, and Camera Movement

---
+++ flightDowntown/Assets/Scripts/cameraScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScript : MonoBehaviour
+{
+
+    Rigidbody2D rbody;
+    float x;
+    float y;
+    public heroScript hero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = gameObject.GetComponent<Rigidbody2D>();
+        rbody.velocity = new Vector2(5, 0);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = gameObject.GetComponent<Transform>().position.x -5;
+        y = hero.GetComponent<Transform>().position.y;
+
+        hero.GetComponent<Transform>().position = new Vector2(x, y);
+
+    }
+}
---
+++ flightDowntown/Assets/Scripts/heroScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heroScript : MonoBehaviour
+{
+    Rigidbody2D rbody;
+    public ceilingScript ceiling;
+    float x;
+    float y;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = gameObject.GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rbody.AddForce(new Vector2(0, 300));
+        }
+
+        x = gameObject.GetComponent<Transform>().position.x;
+        y = ceiling.GetComponent<Transform>().position.y;
+        ceiling.GetComponent<Transform>().position = new Vector2(x, y);
+
+
+
+    }
+}
