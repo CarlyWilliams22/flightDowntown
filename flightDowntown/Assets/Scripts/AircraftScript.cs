db3673c30f2cd5dc5256639e0aac4dab3b229d1a
CarlyWilliams22 <57012119+CarlyWilliams22@users.noreply.github.com>
2020-09-26 18:30:42 -0400

    helicopters move slowly forward

--- flightDowntown/Assets/Scripts/AircraftScript.cs
+++ flightDowntown/Assets/Scripts/AircraftScript.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class AircraftScript : MonoBehaviour
 {
+
+    Rigidbody2D rbody;
+
     // Start is called before the first frame update
     void Start()
     {
+        rbody = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        rbody.velocity = new Vector2(-1, 0);
         
     }
 }
